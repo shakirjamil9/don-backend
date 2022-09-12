072b8fbe5f9b0fe53f9a168ad6c6c27401f8e5bb
shakirjamil9 <mianshakir9@hotmail.com>
2022-09-13 02:51:28 +0500

    delete tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -64,12 +64,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  const { id } = req.params;
-  const tour = tourData.find((el) => el.id == id);
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      err,
+    });
+  }
 };
